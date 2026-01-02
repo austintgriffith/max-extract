829ea02a6494ff121a5fdefaee1758cfd3c9caaa
Austin Griffith <austin@ethereum.org>
2026-01-02 11:24:11 -0700

    upgrade the unfurl

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -8,7 +8,7 @@ const titleTemplate = "%s | Max Extract Protocol";
 export const getMetadata = ({
   title,
   description,
-  imageRelativePath = "/thumbnail.jpg",
+  imageRelativePath = "/maxwider.jpg",
 }: {
   title: string;
   description: string;
@@ -32,10 +32,14 @@ export const getMetadata = ({
       images: [
         {
           url: imageUrl,
+          width: 1200,
+          height: 630,
+          type: "image/jpeg",
         },
       ],
     },
     twitter: {
+      card: "summary_large_image",
       title: {
         default: title,
         template: titleTemplate,
